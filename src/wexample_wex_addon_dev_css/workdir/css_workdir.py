690eeb2f0d85de47cbbfff931272690cb309850a
weeger <romain.weeger@gmail.com>
2025-08-29 09:41:50 +0200

    Rectify

--- src/wexample_wex_addon_dev_css/workdir/css_workdir.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from wexample_wex_core.workdir.code_base_workdir import CodeBaseWorkdir
-
-
-class CssWorkdir(CodeBaseWorkdir):
-    """Base workdir for CSS projects (stub)."""
-    pass

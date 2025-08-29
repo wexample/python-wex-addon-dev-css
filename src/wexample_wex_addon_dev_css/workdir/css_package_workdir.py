4488143f723a9d11eb1377ba1eaf76b7e3ef6d38
weeger <romain.weeger@gmail.com>
2025-08-29 09:12:06 +0200

    Base config

--- src/wexample_wex_addon_dev_css/workdir/css_package_workdir.py
+++ src/wexample_wex_addon_dev_css/workdir/css_package_workdir.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+from wexample_config.const.types import DictConfig
 from wexample_wex_addon_dev_css.workdir.css_workdir import CssWorkdir
 
 class CssPackageWorkdir(CssWorkdir):
-    """Workdir placeholder for a CSS package."""
-    pass
+    def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
+
+        raw_value = super().prepare_value(raw_value=raw_value)
+
+        return raw_value
--- src/wexample_wex_addon_dev_css/workdir/css_workdir.py
+++ src/wexample_wex_addon_dev_css/workdir/css_workdir.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
-class CssWorkdir:
+from wexample_wex_core.workdir.code_base_workdir import CodeBaseWorkdir
+
+
+class CssWorkdir(CodeBaseWorkdir):
     """Base workdir for CSS projects (stub)."""
     pass
